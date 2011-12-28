a60c6c0e9713d7f706d9ffe4109a92ba01deca57
ahomu <mail@ayumusato.com>
2011-12-28 20:24:33 +0900

    queryAllと_selectorに分離

--- hayate.js
+++ hayate.js
@@ -10,7 +10,16 @@ Hayate || (function(win, doc, loc, nav) {
     Hayate = queryAll;
 
     var oldIE   = /MSIE [678]/.test(nav.userAgent),
-        toArray = !!oldIE ? toArrayClone : toArraySlice;
+        toArray = !!oldIE ? toArrayClone : toArraySlice,
+        IE_FIX_ATTR = {
+            'class'     : 'className',
+            'for'       : 'htmlFor',
+            acesskey    : 'accessKey',
+            tabindex    : 'tabIndex',
+            colspan     : 'colSpan',
+            rowspan     : 'rowSpan',
+            frameborder : 'frameBorder'
+    };
 
     /**
      * ArrayっぽいObject(NodeList, HTMLCollection)を，Arrayに変換
@@ -34,19 +43,24 @@ Hayate || (function(win, doc, loc, nav) {
         return Array.prototype.slice.call(list);
     }
 
+    function concatArray(array, list) {
+        if (!oldIE) {
+            return array.concat(Array.prototype.slice.call(list));
+        } else {
+            var j = array.length, i = list.length;
+            for (; i-->0;) {
+                if (i in list) {
+                    array[j++]= list[i];
+                }
+            }
+            return array;
+        }
+    }
+
     function queryAll(expr, root) {
-        var exprStack, i, iz,
-            exprGroup, j, jz,
-            exprToken,
-            RE_STANDARD   = /^([a-z]*)([.#]?[a-zA-Z0-9-_]*)$/,
-            RE_COMBINATOR = /^([>+~])?$/,
-            RE_ATTRIBUTE  = /^\[(.+?)([~^$*|!>])=(.+?)\]$/,
-            RE_PSEUDOS    = /^:([a-z-]+)\(?([a-z-+\d]*)\)?$/,
-            RE_CONCISE    = /^([.#]?)[\w\-_]+$/,
-            matches,
-            evaluating,
-            processing,
-            rv = [];
+        var exprStack, i,
+            RE_CONCISE = /^([.#]?)[\w\-_]+$/,
+            rv;
 
         root = root || doc;
 
@@ -54,6 +68,31 @@ Hayate || (function(win, doc, loc, nav) {
         if (RE_CONCISE.test(expr)) {
             return _concise(expr, root);
         }
+
+        if (expr.indexOf(',') === -1) {
+            return _selector(expr, root);
+        } else {
+            exprStack = expr.split(',');
+
+            rv = [];
+            i  = 0;
+            while (expr = exprStack[i++]) {
+                rv = rv.concat(_selector(expr, root));
+            }
+            return rv;
+        }
+    }
+
+    function _selector(expr, root) {
+        var i = 0,
+            tokenGroup,
+            token,
+            RE_STANDARD   = /^([a-z]*)([.#]?[a-zA-Z0-9-_]*)$/,
+            RE_COMBINATOR = /^([>+~])?$/,
+            RE_ATTRIBUTE  = /^\[(.+?)([~^$*|!>])=(.+?)\]$/,
+            RE_PSEUDOS    = /^:([a-z-]+)\(?([a-z-+\d]*)\)?$/,
+            matches,
+            rv;
 
         // IDがあればそれ以前を切り取る
         if (expr.indexOf('#')) {
@@ -74,104 +113,68 @@ Hayate || (function(win, doc, loc, nav) {
             .replace(/\s?\[/g, ' [')
             // pseudosの左脇にスペース
             .replace(/\s?:/g, ' :')
-            // 複数指定のカンマ区切りの両脇をトリム
-            .replace(/\s?,\s?/g, ',')
+            // trim
+            .replace(/^\s*|\s*$/g, '')
         ;
 
-        if (expr.indexOf(',')) {
+        // セレクタを評価単位に分割
+        tokenGroup = expr.split(' ');
 
-        }
+        // 評価中変数を初期化
+        rv = [root];
 
-        // 複数のセレクタに分解
-        exprStack = expr.split(',');
+        while (token = tokenGroup[i++]) {
 
-        i  = 0;
-        iz = exprStack.length;
-        for (; i<iz; i++) {
+            switch(token.charAt(0)) {
+                case '[':
+                    // attribute
+                    if (matches = RE_ATTRIBUTE.exec(token)) {
+                        // matches[1] attribute
+                        //        [2] operator
+                        //        [3] criterion
+                        rv = _attribute(rv, matches[2], matches[1], matches[3]);
+                    }
+                break;
+                case ':':
+                    // pseudos
+                    if (matches = RE_PSEUDOS.exec(token)) {
+                        // matches[1] pseudo
+                        //        [2] argutment
+                        rv = _pseudos(rv, matches[1], matches[2]);
+                    }
+                break;
+                case '>':
+                case '~':
+                case '+':
+                    // combinator
+                    rv = _combinator(rv, token.charAt(0));
 
-            // セレクタを評価単位に分割
-            exprGroup = exprStack[i].split(' ');
+                    // remove combinator
+                    token = token.substr(1);
 
-            // 評価中変数を初期化
-            evaluating = [root];
-
-            j  = 0;
-            jz = exprGroup.length;
-            for (; j<jz; j++) {
-
-                exprToken = exprGroup[j].trim();
-
-                switch(exprToken.charAt(0)) {
-                    case '[':
-                        // attribute
-                        if (matches = RE_ATTRIBUTE.exec(exprToken)) {
-                            // matches[1] attribute
-                            //        [2] operator
-                            //        [3] criterion
-                            evaluating = _attribute(evaluating, matches[2], matches[1], matches[3]);
-                        }
-                    break;
-                    case ':':
-                        // pseudos
-                        if (matches = RE_PSEUDOS.exec(exprToken)) {
-                            // matches[1] pseudo
-                            //        [2] argutment
-                            evaluating = _pseudos(evaluating, matches[1], matches[2]);
-                        }
-                    break;
-                    case '>':
-                    case '~':
-                    case '+':
-                        evaluating = _combinator(evaluating, exprToken.charAt(0));
-
-                        // fall down
-                        exprToken = exprToken.substr(1);
-                        if (matches = RE_STANDARD.exec(exprToken)) {
-                            // matches[1] element tag
-                            //        [2] class|ident
-                            if (!matches[2]) {
-                                evaluating = _tag(matches[1], evaluating);
-                            } else {
-                                switch (matches[2].charAt(0)) {
-                                    case '#':
-                                        evaluating = _ident(matches[2].substr(1), root, matches[1]);
-                                    break;
-                                    case '.':
-                                        evaluating = _clazz(matches[2].substr(1), evaluating, matches[1]);
-                                    break;
-                                }
+                    // through down
+                default :
+                    if (matches = RE_STANDARD.exec(token)) {
+                        // matches[1] element tag
+                        //        [2] class|ident
+                        if (!matches[2]) {
+                            rv = _tag(matches[1], rv);
+                        } else {
+                            switch (matches[2].charAt(0)) {
+                                case '#':
+                                    rv = _ident(matches[2].substr(1), root, matches[1]);
+                                break;
+                                case '.':
+                                    rv = _clazz(matches[2].substr(1), rv, matches[1]);
+                                break;
                             }
                         }
-                    break;
-                    default :
-                        if (matches = RE_STANDARD.exec(exprToken)) {
-                            // matches[1] element tag
-                            //        [2] class|ident
-                            if (!matches[2]) {
-                                evaluating = _tag(matches[1], evaluating);
-                            } else {
-                                switch (matches[2].charAt(0)) {
-                                    case '#':
-                                        evaluating = _ident(matches[2].substr(1), root, matches[1]);
-                                    break;
-                                    case '.':
-                                        evaluating = _clazz(matches[2].substr(1), evaluating, matches[1]);
-                                    break;
-                                }
-                            }
-                        }
-                    break;
-                }
+                    }
+                break;
             }
-            // 結果に結合
-            rv = rv.concat(evaluating);
         }
 
         return rv;
-    }
-
-    function _selector(expr, root) {
-
     }
 
     function _concise(expr, root) {
@@ -193,7 +196,7 @@ Hayate || (function(win, doc, loc, nav) {
     /**
      *
      * @param {String} ident
-     * @param {Array} root
+     * @param {Element} root
      * @param {String} [tagName]
      */
     function _ident(ident, root, tagName) {
@@ -220,16 +223,13 @@ Hayate || (function(win, doc, loc, nav) {
     function _clazz(clazz, rootAry, tagName) {
         tagName  = tagName && tagName.toUpperCase();
 
-        var rvStack = [], i = 0, root;
+        var rv = [], i = 0, root, tmp;
 
         while (root = rootAry[i++] ) {
-            if (root.getElementsByClassName) {
-                rvStack = rvStack.concat(toArray(root.getElementsByClassName(clazz)));
-            } else {
+            if (!root.getElementsByClassName) {
                 var elms    = root.getElementsByTagName('*'),
                     evClass = [' ', clazz, ' '].join(''),
                     elClass = [' ', , ' '],
-                    rv      = [],
                     j       = 0,
                     e;
 
@@ -241,19 +241,31 @@ Hayate || (function(win, doc, loc, nav) {
                         }
                     }
                 }
-                rvStack = rvStack.concat(rv);
+            } else {
+                tmp = toArray(root.getElementsByClassName(clazz));
+                if (tagName) {
+                    var k = 0, r;
+                    while (r = tmp[k++]) {
+                        if (r.tagName === tagName) {
+                            rv.push(r);
+                        }
+                    }
+                } else {
+                    rv = rv.concat(tmp);
+                }
             }
         }
-        return rvStack;
+        return rv;
     }
 
     function _tag(tagName, rootAry) {
-        var rvStack = [], i = 0, root;
+        var rv = [], i = 0, root;
 
         while (root = rootAry[i++]) {
-            rvStack = rvStack.concat(toArray(root.getElementsByTagName(tagName)));
+            rv = concatArray(rv, root.getElementsByTagName(tagName));
         }
-        return rvStack;
+
+        return rv;
     }
 
     function _attribute(elms, opr, attr, crit) {
@@ -265,42 +277,42 @@ Hayate || (function(win, doc, loc, nav) {
         switch (opr) {
             case '' :
                 while (e = elms[i++]) {
-                    if (e.getAttribute(attr) === crit) {
+                    if (getAttr(e, attr) === crit) {
                         rv.push(e);
                     }
                 }
                 break;
             case '$':
                 while (e = elms[i++]) {
-                    if (new RegExp(crit+'$').test(e.getAttribute(attr))) {
+                    if (new RegExp(crit+'$').test(getAttr(e, attr))) {
                         rv.push(e);
                     }
                 }
                 break;
             case '^':
                 while (e = elms[i++]) {
-                    if (e.getAttribute(attr).indexOf(crit) === 0) {
+                    if (getAttr(e, attr).indexOf(crit) === 0) {
                         rv.push(e);
                     }
                 }
                 break;
             case '*':
                 while (e = elms[i++]) {
-                    if (e.getAttribute(attr).indexOf(crit) !== -1) {
+                    if (getAttr(e, attr).indexOf(crit) !== -1) {
                         rv.push(e);
                     }
                 }
                 break;
             case '~':
                 while (e = elms[i++]) {
-                    if ((' '+e.getAttribute(attr)+' ').indexOf(crit) !== -1) {
+                    if ((' '+getAttr(e, attr)+' ').indexOf(crit) !== -1) {
                         rv.push(e);
                     }
                 }
                 break;
             case '|':
                 while (e = elms[i++]) {
-                    if (e.getAttribute(attr) === crit) {
+                    if (getAttr(e, attr) === crit) {
                         rv.push(e);
                     }
                 }
@@ -317,4 +329,10 @@ Hayate || (function(win, doc, loc, nav) {
         return elms;
     }
 
+    function getAttr(elm, key) {
+        if (!!oldIE && key in IE_FIX_ATTR) {
+            key = IE_FIX_ATTR[key];
+        }
+        return elm.getAttribute(key);
+    }
 })(window, document, location, navigator);
